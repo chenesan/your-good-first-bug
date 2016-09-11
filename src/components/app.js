cf307e7608325540f61de54790e468fea94e8616
陳乙山 <yishan@simpleinfo.cc>
2016-09-11 17:18:34 +0800

    Add top DOM and style

--- src/components/app.js
+++ src/components/app.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div>Hello world!</div>;
+    return (
+      <div className="main-app">
+        <div className="main-jumbotron">jumbotron</div>
+        <div className="main-content">content</div>
+        <div className="main-menu">menu</div>
+      </div>
+    );
   }
 }
 
