77b74f611ea40c7a04310b70d91f3762574718b4
陳乙山 <yishan@simpleinfo.cc>
2016-09-13 11:50:06 +0800

    Add slide menu

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,41 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.handleClickOnMenuButton = this.handleClickOnMenuButton.bind(this);
+    this.state = {
+      showMenu: false,
+    };
+  }
+  handleClickOnMenuButton() {
+    if (!this.state.showMenu) {
+      this.setState(Object.assign({}, this.state, {
+        showMenu: true,
+      }));
+    } else {
+      this.setState(Object.assign({}, this.state, {
+        showMenu: false,
+      }));
+    }
+  }
   render() {
     return (
-      <div className="main-app">
+      <div className="main-app container">
         <div className="main-jumbotron">jumbotron</div>
-        <div className="main-content">content</div>
-        <div className="main-menu">menu</div>
+        <div className="main-content">
+          <div className="main-menu col-sm-2">menu</div>
+          <div className="content col-xs-12 col-sm-5">content1</div>
+          <div className="content col-xs-12 col-sm-5">content2</div>
+        </div>
+        <div
+          className={`main-menu col-xs-4 -side${this.state.showMenu ? '' : ' -hide'}`}
+          onClick={this.handleClickOnMenuButton}
+        >menu</div>
+        <div
+          className={`menu-button${this.state.showMenu ? ' -hide' : ' -side'}`}
+          onClick={this.handleClickOnMenuButton}
+        >button</div>
       </div>
     );
   }
