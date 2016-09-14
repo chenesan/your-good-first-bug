af7be3438f8bb54338d4abe17c472d96c44f350b
陳乙山 <yishan@simpleinfo.cc>
2016-09-14 17:27:37 +0800

    Seperate bug item component

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import BugItem from './bug-item';
 import Menu from './menu';
 
 class App extends Component {
@@ -29,26 +30,7 @@ class App extends Component {
           <div className="content col-xs-12 col-sm-10">
             {
               this.props.bugList.map((bugData) => (
-                <article className="bug-item col-xs-12 col-sm-5">
-                  <h3 className="title">{bugData.title}</h3>
-                  <footer className="metadata">
-                    <span className="organizer">{bugData.organizer}</span>
-                    /
-                    <span className="project">{bugData.project}</span>
-                    <div className="languages">
-                    {
-                      bugData.languages.map(
-                          (language) => <span className="language">{language}</span>
-                        )
-                      }
-                    </div>
-                    <address className="sources">
-                      <a href={bugData.url} className="issue">Issue source</a>
-                      <a href={bugData.projectUrl} className="project">project source</a>
-                    </address>
-                    <time className="timestamp">{bugData.timestamp}</time>
-                  </footer>
-                </article>
+                <BugItem bugData={bugData} />
               ))
             }
           </div>
---
+++ src/components/bug-item.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class BugItem extends Component {
+  render() {
+    return (
+      <article className="bug-item col-xs-12 col-sm-5">
+        <h3 className="title">{this.props.bugData.title}</h3>
+        <footer className="metadata">
+          <span className="organizer">{this.props.bugData.organizer}</span>
+          /
+          <span className="project">{this.props.bugData.project}</span>
+          <div className="languages">
+          {
+            this.props.bugData.languages.map(
+                (language) => <span className="language">{language}</span>
+              )
+            }
+          </div>
+          <address className="sources">
+            <a href={this.props.bugData.url} className="issue">Issue source</a>
+            <a href={this.props.bugData.projectUrl} className="project">project source</a>
+          </address>
+          <time className="timestamp">{this.props.bugData.timestamp}</time>
+        </footer>
+      </article>
+    );
+  }
+}
+
+BugItem.propTypes = {
+  bugData: React.PropTypes.shape({
+    languages: React.PropTypes.arrayOf(() => true),
+    title: React.PropTypes.string.isRequired,
+    organizer: React.PropTypes.string.isRequired,
+    project: React.PropTypes.string.isRequired,
+    projectUrl: React.PropTypes.string.isRequired,
+    timestamp: React.PropTypes.instanceOf(Date),
+    url: React.PropTypes.string.isRequired,
+  }),
+};
+
+export default BugItem;
