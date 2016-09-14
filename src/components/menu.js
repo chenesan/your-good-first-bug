d56ff592dcf9c6084b42c001c5d752fc47a8b484
陳乙山 <yishan@simpleinfo.cc>
2016-09-14 17:28:20 +0800

    Add content of menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -17,9 +17,46 @@ class Menu extends Component {
   }
   render() {
     const className = this.getClassName();
-    return (<div
-      className={className}
-    >menu</div>);
+    return (<aside className={className}>
+      <div className="sortBy">
+        <h4 className="title">Sorting by:</h4>
+        <div className="selection">
+          <h5 className="name">Condition:</h5>
+          <select name="condition" className="options">
+            <option value="date">date</option>
+            <option value="project size">project size</option>
+            <option value="popularity">popularity</option>
+          </select>
+        </div>
+        <div className="selection">
+          <h5 className="name">Order:</h5>
+          <select name="order" className="options">
+            <option value="descendant">descendant</option>
+            <option value="ascendant">ascendant</option>
+          </select>
+        </div>
+      </div>
+      <div className="filter">
+        <h4 className="title">Filter:</h4>
+        <div className="selection">
+          <h5 className="name">Language:</h5>
+          <select name="condition" className="options">
+            <option value="all">all</option>
+            <option value="python">python</option>
+            <option value="javascript">javascript</option>
+          </select>
+        </div>
+        <div className="selection">
+          <h5 className="name">Code base size:</h5>
+          <select name="condition" className="options">
+            <option value="all">all</option>
+            <option value="< 1k lines">{"< 1k lines"}</option>
+            <option value="1k ~ 10k lines">1k ~ 10k lines</option>
+            <option value="> 10k lines">{"> 10k lines"}</option>
+          </select>
+        </div>
+      </div>
+    </aside>);
   }
 }
 
