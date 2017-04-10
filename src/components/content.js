b112d705d4f19954c25ddff96cec0739dd9f47bc
陳乙山 <pipio1994@gmail.com>
2017-04-10 10:22:25 +0800

    Add NoResult Component
    When it's not fetching and there's no issues, show NoResult.

---
+++ src/components/content.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import IssueItem from './issue-item';
+import NoResult from './no-result';
+
+export default function Content(props) {
+  let innerContent;
+  if (props.issueList.length === 0 && !props.fetching) {
+    // not fetching and there's no issues
+    // so there's no result.
+    innerContent = <NoResult />;
+  } else {
+    innerContent = props.issueList.map((issueData) => (
+      <IssueItem issueData={issueData} />
+    ));
+  }
+  return <div className="content">{innerContent}</div>;
+}
+
+Content.propTypes = {
+  fetching: React.PropTypes.bool.isRequired,
+  issueList: React.PropTypes.arrayOf((propVal, key) => {
+    // todo: validator
+    if (!propVal.title) {
+      return new Error(`Issue lack of title: ${propVal} with key ${key}`);
+    }
+    return propVal;
+  }),
+};
---
+++ src/components/no-result.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NoResult() {
+  return <h2 className="no-result-tip">Oops! Your query did not match any issues.</h2>;
+}
